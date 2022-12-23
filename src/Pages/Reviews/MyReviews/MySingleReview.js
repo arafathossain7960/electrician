f68a02aa105871ecc7ffcfa50cabd89ae7e702ff
Arafat Hossaine <arafathossain7960@gmail.comm>
2022-12-23 09:23:29 +0600

    handle review delete

--- src/Pages/Reviews/MyReviews/MySingleReview.js
+++ src/Pages/Reviews/MyReviews/MySingleReview.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 const MySingleReview = ({myReview}) => {
     const {_id, email, review}=myReview;
+
+    const handleReviewDelete =(id)=>{
+        alert('Are you confirm this review will delete permanently');
+        fetch(`http://localhost:5000/myReview/${id}`,{method:'DELETE'} )
+       .then(res => res.json())
+       .then(data => console.log(data))
+       
+
+    }
     return (
         <div>
             <small>{email}</small>
             <p>{review}</p>
-            <button>Delete Review</button>
+            <button onClick={()=>handleReviewDelete(_id)}>Delete Review</button>
         </div>
     );
 };
